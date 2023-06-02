c550fb1f259883ee8b0e6b55760b315850331733
mahmud035 <mahamudulhasan4148@gmail.com>
2023-06-02 18:34:32 +0600

    create basic express server and connected to database

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const { MongoClient } = require('mongodb');
+require('colors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//* Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+//* Database Connection
+const url = process.env.URL;
+const client = new MongoClient(url);
+
+const dbConnect = async () => {
+  try {
+    await client.connect();
+
+    console.log(`Database Connected`.yellow.italic);
+
+    app.listen(PORT, () => {
+      console.log(`Server Up and Running`.cyan.bold);
+    });
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold, error.stack);
+  }
+};
+dbConnect();
